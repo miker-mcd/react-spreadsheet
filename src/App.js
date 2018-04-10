522304ff65620b5b56d2b4b6dd3f8a3ebcf9903e
miker-mcd <michaelrmcd@gmail.com>
2018-04-09 19:00:45 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Table from './components/Table'
+import './App.css';
+
+const App = () =>
+  (<div style={{ width: 'max-content' }})>
+    <Table x={4} y={4} />
+  </div>)
+
+export default App;
